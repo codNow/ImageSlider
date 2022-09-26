316ebad563ae8fb8f0eda443b7b94362ab3323e4
codNow <codenow2022@gmail.com>
2022-09-26 22:05:53 +0530

    new loading images

--- app/src/main/java/com/sasha/imageslider/MainActivity.kt
+++ app/src/main/java/com/sasha/imageslider/MainActivity.kt
@@ -2,10 +2,21 @@ package com.sasha.imageslider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
+import com.sasha.slider.ImageSlider
+import com.sasha.slider.constants.ScaleTypes
+import com.sasha.slider.interfaces.ItemChangeListener
+import com.sasha.slider.models.SlideModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
     }
 }
