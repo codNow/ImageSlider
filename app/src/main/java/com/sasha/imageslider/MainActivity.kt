329e67a0aa2106c2e8dd7e89cc32f75f39df8d86
codNow <codenow2022@gmail.com>
2022-10-12 22:02:45 +0530

    new progress bar

--- app/src/main/java/com/sasha/imageslider/MainActivity.kt
+++ app/src/main/java/com/sasha/imageslider/MainActivity.kt
@@ -9,12 +9,24 @@ import com.sasha.slider.interfaces.ItemChangeListener
 import com.sasha.slider.models.SlideModel
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
 
+        val imageList = ArrayList<SlideModel>() // Create image list
+
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(SlideModel("https://bit.ly/2YoJ77H", "The animal population decreased by 58 percent in 42 years."))
+        imageList.add(SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct."))
+        imageList.add(SlideModel("https://bit.ly/3fLJf72", "And people do that."))
+
+        val imageSlider = findViewById<ImageSlider>(R.id.imageSlider)
+        imageSlider.setImageList(imageList)
 
 
 
